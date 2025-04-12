f7ae72e838fa7d8cf2f6f47645543ce7c44cb0c2
Guruvishnu Kurra <guruvishnukurra800@gmail.com>
2025-04-12 20:06:43 +0530

    Fix JournalList import in Journal page

--- src/pages/Journal.tsx
+++ src/pages/Journal.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { JournalEditor } from '@/components/journal/JournalEditor';
-import { JournalList } from '@/components/journal/JournalList';
+import JournalList from '@/components/journal/JournalList';
 import { Button } from '@/components/ui/button';
 
 export function Journal() {
